b27a0e0a78c841cb57b69eac9e13cc613616cbc7
hoon cli <clienteslim@gmail.com>
2021-04-20 17:45:34 +0900

    2021.04.20 17:45 hoon cli
    공모전관리 비활성화시에도 뜨도록 변경 (서블릿->매퍼까지 추가)

--- first_book/src/com/lastbug/firstbook/admin/model/service/AdminContestService.java
+++ first_book/src/com/lastbug/firstbook/admin/model/service/AdminContestService.java
@@ -6,8 +6,12 @@ import static com.lastbug.firstbook.common.jdbc.JDBCTemplate.getConnection;
 import static com.lastbug.firstbook.common.jdbc.JDBCTemplate.rollback;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 import com.lastbug.firstbook.admin.model.dao.AdminContestDAO;
+import com.lastbug.firstbook.contest.model.dto.ContestDTO;
 
 public class AdminContestService {
 
@@ -59,6 +63,67 @@ public class AdminContestService {
 		
 		return result;
 	}
+
+	public List<ContestDTO> selectContestList(int competNum) {
+					
+		Connection con = getConnection();
+		
+		List<ContestDTO> contestList = acDAO.selectContestList(con, competNum);
+		
+		close(con);
+		
+		return contestList;
+	}
+
+	public List<ContestDTO> selectAllconstList() {
+
+		Connection con = getConnection();
+		String month = "";
+		String trueMonth ="";
+		
+		
+		switch (NowMonth()) {
+		case "01"  : trueMonth += 1; break;
+		case "02"  : trueMonth += 1; break;
+		case "03"  : trueMonth += 1; break;
+		case "04"  : trueMonth += 2; break;
+		case "05"  : trueMonth += 2; break;
+		case "06"  : trueMonth += 2; break;
+		case "07"  : trueMonth += 3; break;
+		case "08"  : trueMonth += 3; break;
+		case "09"  : trueMonth += 3; break;
+		case "10"  : trueMonth += 4; break;
+		case "11"  : trueMonth += 4; break;
+		case "12"  : trueMonth += 4; break;
+		}
+		
+		month = NowYear() + "Q" + trueMonth;
+		
+		
+		List<ContestDTO> contestList = acDAO.selectAllContestList(con, month);
+		
+		close(con);
+		
+		return contestList;
+	
+	}
+	
+	
+	// 현재 년도 알아오는 메소드
+		public static String NowYear() {
+			long time = System.currentTimeMillis();
+			SimpleDateFormat dayTime = new SimpleDateFormat("yyyy");
+			String str = dayTime.format(new Date(time));
+			return str; 
+		}
+		
+		// 현재 달 알아오는 메소드 
+		public static String NowMonth() {
+			long time = System.currentTimeMillis(); 
+			SimpleDateFormat dayTime = new SimpleDateFormat("MM");
+			String str = dayTime.format(new Date(time));
+			return str; 
+		}
 	
 	
 }
